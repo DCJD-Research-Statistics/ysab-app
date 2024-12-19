d8adb797f3460194c77f9ff815b588ab42f11195
Juan Lomeli <juan.lomeli@dallascounty.org>
2024-12-19 15:54:06 -0600

    disabled application button

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 class Config:
-    """Base configuration for the app."""
-    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
+    """Base configuration."""
     MONGO_URI = os.getenv("MONGO_URI")
     DB_NAME = os.getenv("DB_NAME")
     DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
     ADMIN_EMAILS = os.getenv("ADMIN_EMAILS", "").split(',')
     DEPUTY_EMAILS = os.getenv("DEPUTY_EMAILS", "").split(',')
 
-class AdminConfig(Config):
+class TestingConfig(Config):
     """Configuration for Admin mode."""
     DB_NAME = os.getenv("DB_NAME_DEV")
     DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL_DEV")
